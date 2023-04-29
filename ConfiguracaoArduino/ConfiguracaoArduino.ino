55c60a34662faf0fb2169479e5fb5d748e6ce4f2
GabrieLima12 <gabrieldocouto.1207@gmail.com>
2023-04-29 20:08:25 -0300

    Iniciando o projeto

---
+++ ConfiguracaoArduino/ConfiguracaoArduino.ino
@@ -0,0 +1,36 @@
+int pino_trigger = 7;
+int pino_echo = 6;
+
+float cm;
+ 
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(pino_echo, INPUT);
+  pinMode(pino_trigger, OUTPUT);
+}
+ 
+void loop()
+{
+  cm = distancia();
+  
+  if (cm < 50)
+  {
+    Serial.print(true);
+    delay(1000);
+  }
+  
+}
+
+float distancia()
+{
+  digitalWrite(pino_trigger, LOW);
+  delayMicroseconds(5);
+  digitalWrite(pino_trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pino_trigger, LOW);
+  
+  float dist = pulseIn(pino_echo, HIGH);
+  dist = dist / 58;
+  return dist;
+}
